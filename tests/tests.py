f4e6d4d513b86a36a7692d8e81af51061ed5f169
Dima Gerasimov <karlicoss@gmail.com>
2018-12-04 23:42:16 +0000

    XXX

--- tests/tests.py
+++ tests/tests.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python3
 import json
 import os
-from os.path import dirname, pardir, join
+from os import listdir
+from os.path import dirname, pardir, join, abspath
 from subprocess import check_output, check_call
 from tempfile import TemporaryDirectory
 from typing import List
 
-import pytest
+import pytest # type: ignore
 
-
-ARCHIVER_BIN = join(dirname(__file__), pardir, 'archive')
-
+ROOT = abspath(join(dirname(__file__), pardir))
+ARCHIVER_BIN = join(ROOT, 'archive')
 
 class Helper:
     def __init__(self, output_dir: str):
         self.output_dir = output_dir
 
-    def run(self, links, env=None, env_defaults=None):
-        if env_defaults is None:
-            env_defaults = {
-                # we don't wanna spam archive.org witin our tests..
-                'SUBMIT_ARCHIVE_DOT_ORG': 'False',
-            }
+    def run(self, links, env=None):
         if env is None:
             env = {}
-
-        env = dict(**env_defaults, **env)
+        # we don't wanna spam archive.org witin our tests..
+        env['SUBMIT_ARCHIVE_DOT_ORG'] = 'False'
 
         jj = []
         for url in links:
@@ -46,12 +41,20 @@ class Helper:
         )
 
 
+def extract_url(index_dir: str):
+    ifile = join(index_dir, 'index.json')
+    with open(ifile, 'r') as fo:
+        j = json.load(fo)
+        return j['url']
+
+
 class TestArchiver:
     def setup(self):
-        # self.tdir = TemporaryDirectory(dir='hello')
-        class AAA:
-            name = 'hello'
-        self.tdir = AAA()
+        class DebugDir:
+            name = 'debug'
+        self.tdir = DebugDir()
+        # self.tdir = TemporaryDirectory()
+        self.helper = Helper(self.output_dir)
 
     def teardown(self):
         pass
@@ -61,31 +64,74 @@ class TestArchiver:
     def output_dir(self):
         return self.tdir.name
 
+    @pytest.mark.skip
     def test_fetch_favicon_false(self):
-        h = Helper(self.output_dir)
-
-        h.run(links=[
+        """
+        Regression test for crash on missing favicon https://github.com/pirate/bookmark-archiver/pull/114
+        """
+        self.helper.run(links=[
             'https://google.com',
         ], env={
             'FETCH_FAVICON': 'False',
         })
         # for now no asserts, good enough if it isn't failing
 
+    # TODO for testing, just transform all my links to localhost??
+    @pytest.mark.skip
     def test_3000_links(self):
         """
         The pages are deliberatly unreachable. The tool should gracefully process all of them even though individual links are failing.
+        Regression test for https://github.com/pirate/bookmark-archiver/pull/115
         """
-        h = Helper(self.output_dir)
-
-        h.run(links=[
-            f'https://localhost:123/whatever_{i}.html' for i in range(3000)
-        ], env={
+        links = [
+            f'http://localhost:123/test_{i}.html' for i in range(3000)
+        ]
+        self.helper.run(links=links, env={
+            # just try and fetch via wget
             'FETCH_FAVICON': 'False',
             'FETCH_SCREENSHOT': 'False',
             'FETCH_PDF': 'False',
             'FETCH_DOM': 'False',
             'CHECK_SSL_VALIDITY': 'False',
         })
+
+        adir = join(self.output_dir, 'archive')
+        archives = listdir(adir)
+        assert len(archives) == len(links)
+
+        archived = {extract_url(join(adir, a)) for a in sorted(archives)}
+        assert archived == set(links)
+
+    def test_double(self):
+        links = [
+            f'http://localhost:123/test_{i}.html' for i in range(10)
+        ]
+        self.helper.run(links=links, env={
+            # just try and fetch via wget
+            'FETCH_FAVICON': 'False',
+            'FETCH_SCREENSHOT': 'False',
+            'FETCH_PDF': 'False',
+            'FETCH_DOM': 'False',
+            'CHECK_SSL_VALIDITY': 'False',
+        })
+
+        self.helper.run(links=list(reversed(links)), env={
+            # just try and fetch via wget
+            'FETCH_FAVICON': 'False',
+            'FETCH_SCREENSHOT': 'False',
+            'FETCH_PDF': 'False',
+            'FETCH_DOM': 'False',
+            'CHECK_SSL_VALIDITY': 'False',
+        })
+
+        adir = join(self.output_dir, 'archive')
+        archives = listdir(adir)
+        assert len(archives) == len(links)
+
+        archived = {extract_url(join(adir, a)) for a in sorted(archives)}
+        assert archived == set(links)
+        # TODO maybe, if we purge it gets overvritten??
+
 
 
 if __name__ == '__main__':
